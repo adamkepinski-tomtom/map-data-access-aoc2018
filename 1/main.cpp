100c84105e029c0857ec86b06dd232b1771a3bd9
Adam Kępiński <adam.kepinski@tomtom.com>
2022-10-04 14:14:46 +0200

    first task

--- 1/main.cpp
+++ 1/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <fstream>
+#include <set>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+
+    int result = 0;
+    int increment = 0;
+    int count = 0;
+    std::set<int> reached_frequencies;
+    bool found = false;
+
+    while (!found) {
+        std::ifstream f("input");
+        while (f >> increment) {
+            result += increment;
+            if (reached_frequencies.contains(result)) {
+                found = true;
+                break;
+            }
+            reached_frequencies.insert(result);
+            count++;
+        }
+    }
+    std::cout << "set size: " << reached_frequencies.size() << " number of iterations: " << count << '\n';
+    std::cout << result << '\n';
     return 0;
 }
